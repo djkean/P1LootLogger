be087a76a061274e74de98ccb04da06191f46e2b
djkean <djkean7@gmail.com>
2023-02-05 16:44:44 -0330

    Fixes to Data + Loop to add all Contents to Table
    - Made spelling/syntax/etc changes to list of items to be written to itemtable in SQL
    - Changed function for writing data to itemtable to loop through every item in the .txt file

--- p1lootlogger/newfile.js
+++ p1lootlogger/newfile.js
@@ -15,7 +15,9 @@ let newData = filedata.split("\n").map((line, i) => {
   let arrayData = line
     .replace("(", "")
     .replace("),", "")
-    .replace("&eacute;", "e")
+    .replaceAll("&eacute;", "e")
+    .replaceAll("&#39;", "'")
+    .replaceAll("\r\n", "")
     .split(", ");
   let nameAndDesc = {
     name: arrayData[1].replaceAll("'", ""),
@@ -27,11 +29,24 @@ let newData = filedata.split("\n").map((line, i) => {
 
 console.log(newData);
 
-connection.execute(
-  "INSERT INTO `test_schema`.`itemtable` (`itemName`,`itemDescription`,`itemValue`) VALUES (?,?,'0')",
+//newData.forEach((val, idx) => console.log(`[idx] ${idx}`, `[val]`, val));
+
+newData.forEach((value, index) => {
+  connection.execute(
+    "INSERT INTO `test_schema`.`itemtable` (`itemName`,`itemDescription`,`itemValue`) VALUES (?,?,'0')",
+    [value.name, value.description],
+    (err, res) => {
+      if (err) console.log("error with query", err);
+      if (res) console.log("res success", res);
+    }
+  );
+});
+
+/* connection.execute(
+  "INSERT INTO `test_schema`.`dummytable` (`itemName`,`itemDescription`,`itemValue`) VALUES (?,?,'0')",
   [newData[0].name, newData[0].description],
   (err, res) => {
     if (err) console.log("error with query", err);
     if (res) console.log("res success", res);
   }
-);
+); */
