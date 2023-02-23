84a7970c691c277692b443cb6317fda2e9104b6f
djkean <djkean7@gmail.com>
2023-02-23 18:49:11 -0330

    Added Chakra Wrapper for Future
    - Wrapped the App component's contents with ChakraProvider, minor change gettgin ready for once backend issue(s) are solved

--- p1lootlogger/src/App.js
+++ p1lootlogger/src/App.js
@@ -4,16 +4,19 @@ import { Nav } from "./shared/components/Nav/Nav.jsx";
 import { HomePage } from "./pages/home";
 import { TablesPage } from "./pages/tables";
 import { ItemTableResults } from "./pages/itemTableResults";
+import { ChakraProvider } from "@chakra-ui/react";
 
 export function App() {
   return (
-    <BrowserRouter>
-      <Nav />
-      <Routes>
-        <Route path="/home" element={<HomePage />} />
-        <Route path="/tables" element={<TablesPage />} />
-        <Route path="/itemtableresults" element={<ItemTableResults />} />
-      </Routes>
-    </BrowserRouter>
+    <ChakraProvider>
+      <BrowserRouter>
+        <Nav />
+        <Routes>
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/tables" element={<TablesPage />} />
+          <Route path="/itemtableresults" element={<ItemTableResults />} />
+        </Routes>
+      </BrowserRouter>
+    </ChakraProvider>
   );
 }
