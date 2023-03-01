387bd35d669172af34199456965218d24b3c12b3
djkean <djkean7@gmail.com>
2023-03-01 14:49:00 -0330

    Working with Chakra for UI
    - Added a lot of WIP/temporary UI designs for the layout of the project

--- p1lootlogger/src/pages/itemTableResults.jsx
+++ p1lootlogger/src/pages/itemTableResults.jsx
@@ -1,9 +1,43 @@
+import { Box, Container, Heading, SimpleGrid, Text, Center, HStack} from "@chakra-ui/react";
 import React from "react";
 import { useEffect, useState } from "react";
-import { stringify, parse } from "flatted"
 
 export const ItemTableResults = () => {
 const [itemTableValues, setItemTableValues] = useState([]);
+
+const itemTableStyles = {
+  px: "1em",
+  py: "1em",
+  bg: "orange.200",
+  color: "green.400",
+  mx: "1em",
+  my: "0.5em",
+  textAlign: "center",
+  ':hover': {
+    color: "red.200",
+    bg: "purple.400"
+  }
+}
+
+const parentItemGridStyles = {
+  p: "0.3em",
+  column: 3,
+  spacing: 1,
+  minChildWidth: "256px",
+  maxW: "3xl",
+  margin: "auto",
+
+}
+
+
+const itemGridStyles = {
+  bg: "orange.200",
+  h: "4em",
+  px: "0.2em",
+  py: "0.1em",
+  borderRadius: " 0.3em",
+  boxShadow: "base",
+}
 
 useEffect(() => {
   function getItemsFromTable() {
@@ -21,5 +55,40 @@ useEffect(() => {
   getItemsFromTable();
 }, []);
 
-  return <p>beans</p>;
+  return <div>
+    <Container as="section" maxW="100hv" maxH="100hv" bg="gray.500">
+      <Center>
+        <Heading my="1em" p="0.75em">All Items</Heading>
+      </Center>
+      <Center>
+        <Text>A list of all items found in PokeOne</Text>
+      </Center>
+      <Center>
+        <Text>If you think any items are missing, please let us know!</Text>
+      </Center>
+      <Center>
+        <Box sx={itemTableStyles} w="564px">
+          <Text>Search bar or something else here later</Text>
+        </Box>
+      </Center>
+        <SimpleGrid sx={parentItemGridStyles}>
+          <Center>
+            <HStack bg="gray.500">
+            <Box sx={itemGridStyles} w="6em">Item Image</Box>
+            <Box sx={itemGridStyles} w="10em">Item Name</Box>
+            <Box sx={itemGridStyles} w="19em" mr="2em">Item Description</Box>
+            </HStack>
+          </Center>
+        </SimpleGrid>
+        <SimpleGrid sx={parentItemGridStyles}>
+          <Center>
+            <HStack bg="gray.500">
+            <Box sx={itemGridStyles} w="6em">Item Image</Box>
+            <Box sx={itemGridStyles} w="10em">Item Name</Box>
+            <Box sx={itemGridStyles} w="19em" mr="2em">Item Description</Box>
+            </HStack>
+          </Center>
+        </SimpleGrid>
+    </Container>
+  </div>;
 };
--- p1lootlogger/src/shared/components/Nav/Nav.jsx
+++ p1lootlogger/src/shared/components/Nav/Nav.jsx
@@ -1,10 +1,32 @@
+import { Box, Flex, Text, Button, Spacer, HStack } from "@chakra-ui/react";
 import React from "react";
 import { Link } from "react-router-dom";
 
 export function Nav() {
-  return ( <>
-  <Link to="/home">Home</Link>
-  <Link to="/tables">Tables</Link>
-  </>
+
+  const navStyles = {
+    w: "9em",
+    h: "3em",
+
+  }
+
+  const navButtonStyles = {
+    //w: "7em",
+    //h: "3em",
+  }
+
+  return ( <Flex as="nav" bg="gray.200" wrap="wrap" gap="2" p="0.3em">
+    <Box sx={navStyles} bg="green.300"><Link to="/home">Home</Link></Box>
+    <Box sx={navStyles} bg="green.300"><Link to="/tables">Tables</Link></Box>
+    <Box sx={navStyles} bg="pink.300"><Link to="/itemTableResults">Items</Link></Box>
+    <Spacer />
+    <HStack>
+      <Box sx={navButtonStyles}><Button colorScheme="yellow">Sign Up</Button></Box>
+      <Box sx={navButtonStyles}><Button colorScheme="yellow">Log In</Button></Box>
+      <Box sx={navButtonStyles}><Button colorScheme="purple">Username</Button></Box>
+      <Box sx={navButtonStyles}><Button colorScheme="purple">Log Out</Button></Box>
+    </HStack>
+
+  </Flex>
   )
 }
