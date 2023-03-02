ccc28cedf01be6365742a1708b0d3281c43f5de5
djkean <djkean7@gmail.com>
2023-03-01 21:15:11 -0330

    Created Table of all Items
    - Queried itemtable for all items and displayed them on a table
    - Cleaned up unused queries from routing file
    - Fetched api with itemtable response and set in state
    - Mapped over table/grid for every index/ID
    - Handful of styling changes

--- p1lootlogger/server/routes/test.js
+++ p1lootlogger/server/routes/test.js
@@ -4,11 +4,6 @@ const chalk = require("chalk");
 const router = express.Router();
 const connection = require("../connect");
 
-/* router.use((req, res, next) => {
-  console.log("Time: ", Date.now());
-  next();
-}); */
-
 // /api route
 router.get("/", (req, res) => {
   res.send("hello from /api");
@@ -16,15 +11,11 @@ router.get("/", (req, res) => {
 
 // /api/test route
 router.get("/test", (req, res) => {
-  connection.query(
-    `SELECT * FROM itemtable WHERE itemID = 1`,
-    [],
-    (err, results) => {
-      if (err) throw err;
-      console.log(results);
-      res.json({ status: 200, error: null, response: results });
-    }
-  );
+  connection.query(`SELECT * FROM itemtable`, [], (err, results) => {
+    if (err) throw err;
+    console.log(results);
+    res.json({ status: 200, error: null, response: results });
+  });
 });
 
 // /api/itemtable route
@@ -32,14 +23,5 @@ router.get("/itemtable", async (req, res) => {
   const query_itemtable = await connection.execute(`SELECT * FROM itemtable`);
   res.json({ items: query_itemtable });
 });
-
-/* connection.execute(
-  `SELECT * FROM itemtable WHERE itemID = '${test}'`,
-  (err, res) => {
-    if (err) console.log("error with query", err);
-    if (res) console.log("res success", res);
-    connection.destroy();
-  }
-); */
 
 module.exports = router;
--- p1lootlogger/src/pages/itemTableResults.jsx
+++ p1lootlogger/src/pages/itemTableResults.jsx
@@ -26,39 +26,36 @@ const parentItemGridStyles = {
   minChildWidth: "256px",
   maxW: "3xl",
   margin: "auto",
-
 }
-
 
 const itemGridStyles = {
   bg: "orange.200",
-  h: "4em",
-  px: "0.2em",
+  h: "4.2em",
+  px: "0.3em",
   py: "0.1em",
   borderRadius: " 0.3em",
   boxShadow: "base",
+  lineHeight: "1.3em",
 }
 
-useEffect(() => {
-  function getItemsFromTable() {
-    const getItemsFromTable = fetch('/api/test', {
-        method: 'GET',
-    })
-    .then((response) => response.json())
-    .then((items) => {
-        console.log('Fetched items from database:', items);
-    });
-    //setItemTableValues(getItemsFromTable);
-    //console.log();
-  }
+const getItemsFromDb = async () => {
+  const itemDbResponse = await fetch("/api/test", { method: "GET" })
+  const itemDbResponseJson = await itemDbResponse.json();
+  setItemTableValues(itemDbResponseJson.response)
+  return itemDbResponseJson.response
+}
 
-  getItemsFromTable();
+useEffect(() => { 
+  getItemsFromDb()
 }, []);
 
+//too many center components, move to regular css for those maybe?
+  if (itemTableValues.length === 0) return <h2> Fetching Items...</h2>
+
   return <div>
-    <Container as="section" maxW="100hv" maxH="100hv" bg="gray.500">
+    <Container as="section" maxW="100hv" maxH="100hv" bg="gray.500" pb="2em">
       <Center>
-        <Heading my="1em" p="0.75em">All Items</Heading>
+        <Heading my="0.5em" p="0.75em">All Items</Heading>
       </Center>
       <Center>
         <Text>A list of all items found in PokeOne</Text>
@@ -67,28 +64,23 @@ useEffect(() => {
         <Text>If you think any items are missing, please let us know!</Text>
       </Center>
       <Center>
-        <Box sx={itemTableStyles} w="564px">
+        <Box sx={itemTableStyles} w="640px">
           <Text>Search bar or something else here later</Text>
         </Box>
       </Center>
-        <SimpleGrid sx={parentItemGridStyles}>
+      {itemTableValues.length > 0 && itemTableValues.map((item) => {
+        return (
+          <SimpleGrid key={item.itemID} sx={parentItemGridStyles}>
           <Center>
             <HStack bg="gray.500">
             <Box sx={itemGridStyles} w="6em">Item Image</Box>
-            <Box sx={itemGridStyles} w="10em">Item Name</Box>
-            <Box sx={itemGridStyles} w="19em" mr="2em">Item Description</Box>
+            <Box sx={itemGridStyles} w="8em">{item.itemName}</Box>
+            <Box sx={itemGridStyles} w="25em" mr="2em">{item.itemDescription}</Box>
             </HStack>
           </Center>
         </SimpleGrid>
-        <SimpleGrid sx={parentItemGridStyles}>
-          <Center>
-            <HStack bg="gray.500">
-            <Box sx={itemGridStyles} w="6em">Item Image</Box>
-            <Box sx={itemGridStyles} w="10em">Item Name</Box>
-            <Box sx={itemGridStyles} w="19em" mr="2em">Item Description</Box>
-            </HStack>
-          </Center>
-        </SimpleGrid>
+      )
+      })}
     </Container>
   </div>;
 };
