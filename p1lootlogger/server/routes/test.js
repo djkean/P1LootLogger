ab2b52d076f3cd08442fa69c326fb670fe922bba
djkean <djkean7@gmail.com>
2023-03-29 02:31:26 -0230

    WIP Attempt to Refactor createaccount  + use cors

--- p1lootlogger/server/routes/test.js
+++ p1lootlogger/server/routes/test.js
@@ -1,8 +1,13 @@
 require("dotenv").config();
 const express = require("express");
 const chalk = require("chalk");
+const app = express();
 const router = express.Router();
 const connection = require("../connect");
+const cors = require("cors");
+
+app.use(express.json());
+app.use(cors);
 
 // /api route
 router.get("/", (req, res) => {
@@ -27,9 +32,40 @@ router.get("/boss", (req, res) => {
   });
 });
 
-// signup
+// createaccount
+app.post("/createaccount", (req, res) => {
+  const username = req.body.username;
+  const email = req.body.email;
+  const password = req.body.password;
+
+  connection.execute(
+    "INSERT INTO `test_schema`.`dummytable` (`username`,`email`,`password`) VALUES (?,?,?)",
+    [username, email, password],
+    (err, res) => {
+      if (err) console.log("createaccount query error", err);
+      if (res) console.log("createaccount res success", res);
+    }
+  );
+});
+
+/*
+app.post("/createaccount", (req, res) => {
+  const username = req.body.username;
+  const email = req.body.email;
+  const password = req.body.password;
+
+  connection.query(
+    "INSERT INTO usertable2 (username, email, password) VALUES (?,?,?)",
+    [(username, email, password)],
+    (err, results) => {
+      if (err) throw err;
+      console.log(results);
+    }
+  );
+}); */
+
 /* router.post("/createaccount", (req, res) => {
   connection.query = "INSERT INTO usertable2 (`username`,`email`,`password`) VALUES (?,?,?)"
-}) */
+}) 
 
-module.exports = router;
+module.exports = router; */
--- p1lootlogger/src/pages/createaccount.jsx
+++ p1lootlogger/src/pages/createaccount.jsx
@@ -2,9 +2,57 @@ import React, {useState} from "react";
 import { Box, Container, Heading, Text, Center, Input, Button, FormControl, FormLabel} from "@chakra-ui/react";
 import { ButtonStyles, inputStyles } from "../components/pagestyles"
 import { Link } from "react-router-dom"
+import axios from "axios"
 import { regVerification } from "../components/regVerification";
 
 export function CreateAccountPage() {
+
+const [registerUsername, setRegisterUsername] = useState("")
+const [registerEmail, setRegisterEmail] = useState("")
+const [registerPassword, setRegisterPassword] = useState("")
+
+const registerAccount = () => (axios.post("http://localhost8080/createaccount", {
+    username: registerUsername,
+    email: registerEmail,
+    password: registerPassword
+  }).then((err) => console.log(err)))
+
+return (
+  <Container as="section" maxW="100hv" maxH="100hv" bg="#5D5D5D" pb="2em">
+    <Center>
+      <Heading my="1em" p="0.75em">Create an Account</Heading>
+    </Center>
+    <Center>
+      <Text my="1em">To make an account, fill out the fields below:</Text>
+    </Center>
+    <Center>
+      <Text my="1em">Already have an account? Log in </Text><Text color="#FDCA40">
+      <Link to="/login"> here.</Link></Text>
+    </Center>
+      <Center>
+      <Box as="form" id="reg--form">
+        <FormControl>
+          <FormLabel>Username:</FormLabel>
+          <Input sx={inputStyles} type="text" id="reg--username" name="username" onChange={(e) => {setRegisterUsername(e.target.value)}}/>
+          <FormLabel>Email Address:</FormLabel>
+          <Input sx={inputStyles} type="email" id="reg--email--1" name="email" onChange={(e) => {setRegisterEmail(e.target.value)}}/>
+          <FormLabel>Password:</FormLabel>
+          <Input sx={inputStyles} type="password" id="reg--password--1" placeholder="password" name="password" onChange={(e) => {setRegisterPassword(e.target.value)}}/>
+          <Button sx={ButtonStyles} my="1em" type="submit" onClick={registerAccount}>Register</Button>
+        </FormControl>
+      </Box>
+    </Center>
+  </Container>
+
+)
+
+
+
+}
+
+
+
+/* export function CreateAccountPage() {
   const [regDetails, setRegDetails] = useState({
     username: "",
     email: "",
@@ -34,7 +82,7 @@ export function CreateAccountPage() {
       </Center>
       <Center>
         <Text my="1em">Already have an account? Log in </Text><Text color="#FDCA40">
-          <Link to="/login"> here.</Link></Text>
+        <Link to="/login"> here.</Link></Text>
       </Center>
       <Center>
         <Box as="form" id="reg--form" onSubmit={submitFields}>
@@ -55,7 +103,7 @@ export function CreateAccountPage() {
     </Container>
   </div>
   )
-}
+} */
 
 /* <FormLabel>Confirm Email Address:</FormLabel>
   <Input sx={inputStyles} type="email" id="reg--email--2"/>
