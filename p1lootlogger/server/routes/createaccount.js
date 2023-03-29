acbfeafac6f037bb4d82b684e1a2f070d8d848c0
djkean <djkean7@gmail.com>
2023-03-29 02:57:54 -0230

    WIP Corrections to Refactoring
    - Created own routes file for /createaccount
    - Fixed an issue where an export was commented out

--- p1lootlogger/server/routes/createaccount.js
+++ p1lootlogger/server/routes/createaccount.js
@@ -1,36 +1,11 @@
 require("dotenv").config();
 const express = require("express");
-const chalk = require("chalk");
 const app = express();
-const router = express.Router();
 const connection = require("../connect");
 const cors = require("cors");
 
 app.use(express.json());
 app.use(cors());
-
-// /api route
-router.get("/", (req, res) => {
-  res.send("hello from /api");
-});
-
-// /api/test route
-router.get("/test", (req, res) => {
-  connection.query(`SELECT * FROM itemtable`, [], (err, results) => {
-    if (err) throw err;
-    console.log(results);
-    res.json({ status: 200, error: null, response: results });
-  });
-});
-
-// bosstable route
-router.get("/boss", (req, res) => {
-  connection.query(`SELECT * FROM bosstable`, [], (err, results) => {
-    if (err) throw err;
-    console.log(results);
-    res.json({ status: 200, error: null, response: results });
-  });
-});
 
 // createaccount
 app.post("/createaccount", (req, res) => {
@@ -68,4 +43,3 @@ app.post("/createaccount", (req, res) => {
   connection.query = "INSERT INTO usertable2 (`username`,`email`,`password`) VALUES (?,?,?)"
 }) 
 */
-module.exports = router;
