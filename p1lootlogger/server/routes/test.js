a57b375ba4c624aa45ec8c64788aa5f8ec48a34d
djkean <djkean7@gmail.com>
2023-05-21 20:43:00 -0230

    Re-adding .gitignore files to clone  + more
    - New computer mishaps, had to re-add .env, reinstall node modules, etc
    - Added SQL from a friend's database (with permission) to scrape for specific data I intend to use for a future table.
    - Having trouble getting started with node again, added a route to /bossinfo. The nature of the route is intended to change at some point once I create values for specific bosses and have info to display on them.

--- p1lootlogger/server/routes/test.js
+++ p1lootlogger/server/routes/test.js
@@ -27,6 +27,15 @@ router.get("/boss", (req, res) => {
   });
 });
 
+// bossinfo route
+router.get("bossinfo", (req, res) => {
+  connection.query(`SELECT * FROM bossinfotable`, [], (err, results) => {
+    if (err) throw err;
+    console.log(results);
+    res.json({status: 200, error: null, response, results});
+  })
+})
+
 // signup
 /* router.post("/createaccount", (req, res) => {
   connection.query = "INSERT INTO usertable2 (`username`,`email`,`password`) VALUES (?,?,?)"
