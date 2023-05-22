1b383474a8cb96db991e8071f4ab565e785ce1fa
djkean <djkean7@gmail.com>
2023-05-22 03:12:01 -0230

    Fixed api + response JSON error
    - Fixed a couple typo/syntax errors regarding the bossinfo api fetch and response
    - Fixed Chakra UI styling typo
    - Mapped through and displayed (some) data from new bossinfo table, along with adding more data to pull from for examples
    - Need to consider a palatable way to display the data, with a layout that makes sense

--- p1lootlogger/server/sendBossInfo.js
+++ p1lootlogger/server/sendBossInfo.js
@@ -6,183 +6,151 @@ const chalk = require("chalk")
 
 const TEAM_DATA = [
 	{
-		"Pokemon": "Pikachu", 
-		"Nature": "Jolly", 
-		"Move-1": "Focus Punch",
-		"Move-2": "Knock Off",
-		"Move-3": "Volt Tackle",
-		"Move-4": "Extreme Speed"
-	}, 
-	{
-		"Pokemon": "Pikachu", 
+		"Pokemon": "Ninetales (Alola)",
 		"Nature": "Timid",
-		"Move-1": "Nasty Plot",
-		"Move-2": "Thunderbolt",
-		"Move-3": "Grass Knot",
-		"Move-4": "Hidden Power Ice"
-	},
-	{
-		"Pokemon": "Pikachu",
-		"Nature": "Lonely",
-		"Move-1": "Knock Off",
-		"Move-2": "Extreme Speed",
-		"Move-3": "Thunderbolt",
-		"Move-4": "Surf"
-	},
-	{
-		"Pokemon": "Heracross",
-		"Nature": "Jolly",
-		"Move-1": "Megahorn",
-		"Move-2": "Knock Off",
-		"Move-3": "Facade",
-		"Move-4": "Close Combat"
-	},
-	{
-		"Pokemon": "Pidgeot",
-		"Nature": "Jolly",
-		"Move-1": "Brave Bird",
-		"Move-2": "U-Turn",
-		"Move-3": "Pursuit",
-		"Move-4": "Roost"
-	},
-	{
-		"Pokemon": "Gyarados",
-		"Nature": "Impish",
-		"Move-1": "Dragon Tail",
-		"Move-2": "Stone Edge",
-		"Move-3": "Bounce",
-		"Move-4": "Dragon Dance"
-	},
-	{
-		"Pokemon": "Snorlax",
-		"Nature": "Careful",
-		"Move-1": "Ice Punch",
-		"Move-2": "Dynamic Punch",
-		"Move-3": "Crunch",
-		"Move-4": "Body Slam"
-	},
-	{
-		"Pokemon": "Kingler",
-		"Nature": "Jolly",
-		"Move-1": "Swords Dance",
-		"Move-2": "Liquidation",
-		"Move-3": "Knock Off",
-		"Move-4": "X-Scissor"
-	},
-	{
-		"Pokemon": "Lapras",
-		"Nature": "Modest",
 		"Move-1": "Freeze-Dry",
-		"Move-2": "Hydro Pump",
-		"Move-3": "Ice Beam",
-		"Move-4": "Blizzard"
+		"Move-2": "Ice Beam",
+		"Move-3": "Aurora Veil",
+		"Move-4": "Hypnosis"
 	},
 	{
-		"Pokemon": "Feraligatr",
+		"Pokemon": "Mamoswine",
+		"Nature": "Adamant",
+		"Move-1": "Icicle Crash",
+		"Move-2": "Knock Off",
+		"Move-3": "Earthquake",
+		"Move-4": "Swords Dance"
+	},
+	{
+		"Pokemon": "Garchomp",
 		"Nature": "Jolly",
-		"Move-1": "Dragon Dance",
-		"Move-2": "Liquidation",
+		"Move-1": "Stone Edge",
+		"Move-2": "Earthquake",
+		"Move-3": "Swords Dance",
+		"Move-4": "Dragon Claw"
+	},
+	{
+		"Pokemon": "Tyranitar",
+		"Nature": "Adamant",
+		"Move-1": "Swords Dance",
+		"Move-2": "Stone Edge",
 		"Move-3": "Ice Punch",
 		"Move-4": "Crunch"
 	},
 	{
-		"Pokemon": "Typhlosion",
-		"Nature": "Timid",
-		"Move-1": "Eruption",
-		"Move-2": "Focus Blast",
-		"Move-3": "Fire Blast",
-		"Move-4": "Solar Beam"
-	},
-	{
-		"Pokemon": "Meganium",
-		"Nature": "Calm",
-		"Move-1": "Synthesis",
-		"Move-2": "Leech Seed",
-		"Move-3": "Energy Ball",
-		"Move-4": "Toxic"
-	},
-	{
-		"Pokemon": "Venusaur",
-		"Nature": "Bold",
-		"Move-1": "Synthesis",
-		"Move-2": "Grass Knot",
-		"Move-3": "Leech Seed",
-		"Move-4": "Sludge Bomb"
-	},
-	{
-		"Pokemon": "Charizard",
-		"Nature": "Timid",
-		"Move-1": "Roost",
-		"Move-2": "Focus Blast",
-		"Move-3": "Air Slash",
-		"Move-4": "Fire Blast"
-	},
-	{
-		"Pokemon": "Blastoise",
-		"Nature": "Modest",
-		"Move-1": "Hydro Pump",
-		"Move-2": "Ice Beam",
-		"Move-3": "Water Spout",
-		"Move-4": "Scald"
-	},
-	{
-		"Pokemon": "Gyarados (Mega)",
-		"Nature": "Adamant",
-		"Move-1": "Crunch",
-		"Move-2": "Dragon Dance",
-		"Move-3": "Waterfall",
-		"Move-4": "Bounce"
-	},
-	{
-		"Pokemon": "Venusaur (Mega)",
-		"Nature": "Modest",
-		"Move-1": "Knock Off",
-		"Move-2": "Synthesis",
-		"Move-3": "Sludge Bomb",
-		"Move-4": "Giga Drain"
-	},
-	{
-		"Pokemon": "Charizard (Mega X)",
+		"Pokemon": "Gyarados",
 		"Nature": "Jolly",
-		"Move-1": "Roost",
-		"Move-2": "Dragon Claw",
-		"Move-3": "Flare Blitz",
+		"Move-1": "Bounce",
+		"Move-2": "Ice Fang",
+		"Move-3": "Waterfall",
 		"Move-4": "Dragon Dance"
 	},
 	{
-		"Pokemon": "Charizard (Mega Y)",
-		"Nature": "Timid",
+		"Pokemon": "Dragonite",
+		"Nature": "Adamant",
 		"Move-1": "Roost",
-		"Move-2": "Focus Blast",
-		"Move-3": "Solar Beam",
-		"Move-4": "Flamethrower"
+		"Move-2": "Extreme Speed",
+		"Move-3": "Fire Punch",
+		"Move-4": "Outrage"
 	},
 	{
-		"Pokemon": "Blastoise (Mega)",
-		"Nature": "Modest",
-		"Move-1": "Scald",
-		"Move-2": "Dark Pulse",
-		"Move-3": "Ice Beam",
-		"Move-4": "Aura Sphere"
-	},
-	{
-		"Pokemon": "Pidgeot (Mega)",
-		"Nature": "Timid",
-		"Move-1": "Roost",
-		"Move-2": "U-Turn",
-		"Move-3": "Heat Wave",
-		"Move-4": "Hurricane"
-	},
-	{
-		"Pokemon": "Heracross (Mega)",
+		"Pokemon": "Scizor",
 		"Nature": "Adamant",
 		"Move-1": "Swords Dance",
-		"Move-2": "Rock Blast",
-		"Move-3": "Pin Missile",
-		"Move-4": "Close Combat"
+		"Move-2": "Roost",
+		"Move-3": "Bullet Punch",
+		"Move-4": "Bug Bite"
+	},
+	{
+		"Pokemon": "Crobat",
+		"Nature": "Adamant",
+		"Move-1": "Roost",
+		"Move-2": "Toxic",
+		"Move-3": "Brave Bird",
+		"Move-4": "Cross Poison"
+	},
+	{
+		"Pokemon": "Breloom",
+		"Nature": "Jolly",
+		"Move-1": "Seed Bomb",
+		"Move-2": "Rock Tomb",
+		"Move-3": "Mach Punch",
+		"Move-4": "Spore"
+	},
+	{
+		"Pokemon": "Salamence",
+		"Nature": "Jolly",
+		"Move-1": "Dragon Claw",
+		"Move-2": "Fire Blast",
+		"Move-3": "Aqua Tail",
+		"Move-4": "Earthquake"
+	},
+	{
+		"Pokemon": "Metagross",
+		"Nature": "Adamant",
+		"Move-1": "Meteor Mash",
+		"Move-2": "Thunder Punch",
+		"Move-3": "Zen Headbutt",
+		"Move-4": "Ice Punch"
+	},
+	{
+		"Pokemon": "Weavile",
+		"Nature": "Jolly",
+		"Move-1": "Night Slash",
+		"Move-2": "Brick Break",
+		"Move-3": "Ice Shard",
+		"Move-4": "Ice Punch"
+	},
+	{
+		"Pokemon": "Lucario",
+		"Nature": "Jolly",
+		"Move-1": "Extreme Speed",
+		"Move-2": "Swords Dance",
+		"Move-3": "Close Combat",
+		"Move-4": "Bullet Punch"
+	},
+	{
+		"Pokemon": "Drapion",
+		"Nature": "Jolly",
+		"Move-1": "Fire Fang",
+		"Move-2": "Swords Dance",
+		"Move-3": "Crunch",
+		"Move-4": "Poison Jab"
+	},
+	{
+		"Pokemon": "Gallade",
+		"Nature": "Jolly",
+		"Move-1": "Close Combat",
+		"Move-2": "Leaf Blade",
+		"Move-3": "Psycho Cut",
+		"Move-4": "Drain Punch"
+	},
+	{
+		"Pokemon": "Chandelure",
+		"Nature": "Timid",
+		"Move-1": "Shadow Ball",
+		"Move-2": "Fire Blast",
+		"Move-3": "Energy Ball",
+		"Move-4": "Will-O-Wisp"
+	},
+	{
+		"Pokemon": "Galvantula",
+		"Nature": "Timid",
+		"Move-1": "Thunder",
+		"Move-2": "Bug Buzz",
+		"Move-3": "Giga Drain",
+		"Move-4": "Volt Switch"
+	},
+	{
+		"Pokemon": "Krookodile",
+		"Nature": "Jolly",
+		"Move-1": "Dragon Claw",
+		"Move-2": "Dig",
+		"Move-3": "Crunch",
+		"Move-4": "Stone Edge"
 	}
 ];
 
-connection.execute("UPDATE `test_schema`.`bossinfotable2`SET `teamData` = ? WHERE `ID` = 100", [JSON.stringify(TEAM_DATA)], (err, res) => {
+connection.execute("UPDATE `test_schema`.`bossinfotable2`SET `teamData` = ? WHERE `ID` = 102", [JSON.stringify(TEAM_DATA)], (err, res) => {
   if (res) console.log(chalk.cyan("bossinfotable2 was successfully updated"))
 });
--- p1lootlogger/src/pages/bossinfo.jsx
+++ p1lootlogger/src/pages/bossinfo.jsx
@@ -1,6 +1,7 @@
-import {Container, Heading, Text, Center} from "@chakra-ui/react";
+import { Box, Container, Heading, SimpleGrid, Text, Center, HStack} from "@chakra-ui/react";
 import React from "react";
 import { useEffect, useState } from "react";
+import { itemTableStyles, itemGridStyles, parentItemGridStyles } from "../components/pagestyles";
 
 export const BossInfo = () => {
   const [bossInfoValues, setBossInfoValues] = useState([]);
@@ -17,19 +18,34 @@ export const BossInfo = () => {
     getBossInfoFromDb()
   }, []);
 
+  if (bossInfoValues.length === 0) return <h2>fetching Boss Info...</h2>
+
+  bossInfoValues.map((bossData) => console.log(bossData.teamData))
+
   return (
     <div>
-      <Container as="section" maxW="100hv" mawH="100hv" bg="#5D5D5D" pb="2em">
+      <Container as="section" maxW="100hv" maxH="100hv" bg="#5D5D5D" pb="2em">
         <Center>
           <Heading my="0.5em" p="0.75em">(boss name here) - Details</Heading>
         </Center>
         <Center>
           <Text>In-depth information about (boss name here)</Text>
         </Center>
-        <Center>
-          <Text>{bossInfoValues}</Text>
-        </Center>
+         {bossInfoValues.length > 0 && bossInfoValues.map((info) => {
+          return (
+            <SimpleGrid key={info.ID} sx={parentItemGridStyles}>
+              <Center>
+                <HStack>
+                  <Box sx={itemGridStyles} w="6em">Boss Image</Box>
+                  <Box sx={itemGridStyles} w="8em">{info.bossName}</Box>
+                  <Box sx={itemGridStyles} w="25em" mr="2em">{info?.teamData[1]?.Pokemon}</Box>
+                </HStack>
+              </Center>
+            </SimpleGrid>
+          )
+         })}
       </Container>
     </div>
   )
 }
+//{info.teamData[1].Pokemon}
