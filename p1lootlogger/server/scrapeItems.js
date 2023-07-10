0e0beadc8deed8ec1d38381cdf8c13a696c663c3
djkean <djkean7@gmail.com>
2023-07-10 17:25:36 -0230

    Trimmed data from JSON, posted to new Item Table
    - Fixed up JSON object containing item data
         - Removed all null/empty items and objects
         - Removed all BBCode
         - Removed unnecessary \n\n and \t s
         - Removed unnecessary object values for possible future use cases
    
    - Created a scraper to post the item id, name, and description values to a new table

--- p1lootlogger/server/scrapeItems.js
+++ p1lootlogger/server/scrapeItems.js
@@ -2,11 +2,18 @@ const fs = require("fs");
 require("dotenv").config();
 const connection = require("./connect");
 
-// listofitems2 is a txt file of a json object from the game my game is based on
-// and contains all of their data on items which I plan to use to keep the data 
-// as 1 to 1 as possible between this and their projects
-const readItems = fs.readFileSync("listofitems2.txt").toString();
+const readItems = JSON.parse(fs.readFileSync("listofitems2.json"))
+//console.log(readItems)
 
-let formattedData = readItems.split("}").map((line, i) => {
-  let arrayOfItems = line
-})
+for (let i = 0; i < readItems.items.length; i++) {
+  const currentItem = readItems.items[i]
+
+  connection.execute(
+    "INSERT INTO `test_schema`.`newitemtable` (`id`,`name`,`description`) VALUES (?,?,?)",
+    [currentItem.ID, currentItem.Name, currentItem.Description],
+    (err, res) => {
+      if (err) console.log("ERROR", err);
+    })
+
+  //console.log(currentItem.ID, currentItem.Name, currentItem.Description)
+}
