bf446095619ec1cc93c0fad176f3ee0e7f5eae8d
djkean <djkean7@gmail.com>
2023-08-02 22:28:11 -0230

    WIP Cleaning out itemtable
    - Removing WIP search function; Working on one thing at a time (pagination). Having 2 half finished tasks going on at once was a bit confusing

--- p1lootlogger/src/pages/itemtable.jsx
+++ p1lootlogger/src/pages/itemtable.jsx
@@ -6,7 +6,6 @@ import { itemTableStyles, parentItemGridStyles, itemGridStyles } from "../compon
 export const ItemTable = () => {
 const [itemTableValues, setItemTableValues] = useState([]);
 const [currentPage, setCurrentPage] = useState(1);
-const [matchingItems, setMatchingItems] = useState([]);
 
 const getItemsFromDb = async () => {
   const itemDbResponse = await fetch("/api/test", { method: "GET" })
@@ -15,16 +14,6 @@ const getItemsFromDb = async () => {
   return itemDbResponseJson.response
 }
 
-const getMatchingItems = (searchValue) => {
-  const filteredItems = Object.values(itemTableValues).filter(
-    (key) => key.name.toLowerCase().indexOf(searchValue.toLowerCase()) > -1);
-  setMatchingItems(filteredItems);
-};
-
-const handleSearch = (searchValue) => {
-  setCurrentPage(1);
-  return getMatchingItems(searchValue);
-}
 
 const previousPage = currentPage - 1;
 const nextPage = currentPage + 1;
@@ -43,23 +32,11 @@ const testingOn = () => {
   console.log("hello there this button works")
 }
 
-
 useEffect(() => { 
   getItemsFromDb()
 }, []);
 
-//too many center components, move to regular css for those maybe?
-  if (itemTableValues.length === 0) return <h2> Fetching Items...</h2>
-
-  /* if (matchingItems.length === 0) {
-    return (
-      <Container as="section" maxW="100hv" maxH="100hv" bg="#5D5D5D" pb="2em">
-        <Center>
-          <Text>No items matched your search.</Text>
-        </Center>
-      </Container>
-    )
-  } */
+  if (itemTableValues.length === 0) return <h2>Fetching Items...</h2>
 
   return (<div>
     <Container as="section" maxW="100hv" maxH="100hv" bg="#5D5D5D" pb="2em">
