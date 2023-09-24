6b95a3657880abdb9a4618ec11c80946b49ecacf
djkean <djkean7@gmail.com>
2023-09-24 15:33:26 -0230

    Updating User Settings to look less terrible
    - /settings visually the same as other forms used on the site
    - Implementing settings visually as 1 page, but comprised of multiple small forms

--- p1lootlogger/src/pages/settings.jsx
+++ p1lootlogger/src/pages/settings.jsx
@@ -1,23 +1,46 @@
 import { React } from "react";
-import { Box, Checkbox, Flex, FormControl, FormLabel, Heading, Input, Stack, Text } from "@chakra-ui/react";
-import { FormControlColors, InputFieldColors, LoginBox, LoginFlex, LoginStack } from "../components/pagestyles"; 
+import { Box, Center, Flex, FormControl, FormLabel, Heading, Input, Link, Stack, Text } from "@chakra-ui/react";
+import { FormButton, FormControlColors, InputFieldColors, LoginBox, LoginFlex, LoginStack } from "../components/pagestyles"; 
 
 export function SettingsPage() {
   return (
-    <Flex sx={LoginFlex}>
-      <Stack sx={LoginStack} align={"center"}>
-        <Heading>User Settings</Heading>
-        <Text>Here you can adjust settings for your account</Text>
-      </Stack>
-      <Box sx={LoginBox} as="form" id="settings-form">
-        <Stack spacing={3}>
-          <FormControl id="delete-account" sx={FormControlColors}>
-            <FormLabel color={"#FDCA40"}>Delete Account</FormLabel>
-            <Checkbox sx={InputFieldColors} type="radio" id="confirm--del--acc" name="delete--account">
-            </Checkbox>
-          </FormControl>
+    <Flex sx={LoginFlex} align={"center"}>
+      <Stack sx={LoginStack} spacing={6}>
+        <Stack align={"center"}>
+          <Heading fontSize={"3xl"} py={3}>User Settings</Heading>
         </Stack>
-      </Box>
+        <Box sx={LoginBox} id="settings--form">
+          <Stack spacing={3}>
+            <Stack as="form">
+            <FormControl id="username" sx={FormControlColors}>
+              <FormLabel color={"#FDCA40"}>
+                Change Username:</FormLabel>
+                <Text color={"#BFA55C"} align={"left"} py={1}>
+                  Enter your new Username
+                </Text>
+              <Input type="text" sx={InputFieldColors}
+              id="change--username" name="username" />
+               <Center>
+                <Input type="submit" sx={FormButton} value="Change Username"/>
+              </Center>
+            </FormControl>
+            </Stack>
+            <Stack as="form">
+            <FormControl id="delete--account" sx={FormControlColors}>
+              <FormLabel color={"#FDCA40"}>Delete Account:</FormLabel>
+              <Text color={"#BFA55C"} align={"left"} py={1}>
+                Please re-type your Password to confirm
+                </Text>
+              <Input type="password" sx={InputFieldColors}
+              id="ver--password" name="password"/>
+              <Center>
+                <Input type="submit" sx={FormButton} value="Delete my Account"/>
+              </Center>
+            </FormControl>
+            </Stack>
+          </Stack>
+        </Box>
+      </Stack>
     </Flex>
   )
 }
