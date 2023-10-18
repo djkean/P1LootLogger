b0cc256fbbcc1b43febdfd3557f60641fced0a04
djkean <djkean7@gmail.com>
2023-10-18 02:17:00 -0230

    Logout function with axios Request
    - Working on logout() which passes token through header to backend to be destroyed, removes localStorage reference of token, and redirects to /login

--- p1lootlogger/src/components/logout.jsx
+++ p1lootlogger/src/components/logout.jsx
@@ -4,5 +4,14 @@ import { useNavigate } from "react-router-dom";
 import { getUnixTimestamp } from "./getUnixTime";
 
 export function logout() {
-  axios.post("/logout")
+  const navigate = useNavigate()
+  axios.post("/logout", 
+  { headers: { "Authorization": `Bearer ${getToken()}`, "Content-Type": "application/json" } }, 
+  { method: "GET" })
+  .then(res => {
+    console.log("Trying to log out...")
+    localStorage.removeItem("P1LL_TOKEN")
+  })
+  .then(navigate("/login"))
+  .catch(err => console.log("ERROR IN LOGOUT", err))
 }
