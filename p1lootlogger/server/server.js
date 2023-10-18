eb2d7fc9b890ff0dcc1d1ff61b5978b0c4f73cd4
djkean <djkean7@gmail.com>
2023-10-17 23:07:58 -0230

    Tidying up server.js
    - Reorganized imports and variables
    - Moved more variables to global scope to not repeat logic
    - Removed exhaustive console.log()s
    - Updated all res.status()es to take place of removed console.log()s

--- p1lootlogger/server/server.js
+++ p1lootlogger/server/server.js
@@ -2,15 +2,16 @@ require("dotenv").config();
 const express = require("express");
 const chalk = require("chalk");
 const path = require("path");
-const app = express();
-const port = process.env.P1LL_SERVER || 8080;
 const testApi = require("./routes/test");
 const cors = require("cors");
 const connection = require("./connect");
 const jwt = require("jsonwebtoken");
 const crypto = require("crypto");
 
+const app = express();
+const port = process.env.P1LL_SERVER || 8080;
 const secret = process.env.P1LL_SECRETTOKEN
+const salt = crypto.randomBytes(16).toString("hex")
 const usernamePattern = /^[a-zA-Z0-9_-]{3,16}$/
 const passwordPattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9]{8,}$/
 
@@ -25,17 +26,14 @@ const verifyUser = (req, res, next) => {
     const header = req.headers['authorization']
     const token = header && header.split(" ")[1]
     if (token == null) {
-      console.log(chalk.red("TOKEN CHECK FAIL - NO TOKEN"))
-      return res.status(403).json({ message: "Invalid Token" });
+      return res.status(403).json({ message: "403: Invalid Token" });
     }
     jwt.verify(token, secret, (err, decoded) => {
-      console.log(decoded)
       if (err) {
-        console.log(chalk.red("TOKEN AUTH FAIL - INVALID TOKEN"))
-        return res.status(403).json({ message: "Invalid Token" });
+        return res.status(403).json({ message: "403: Invalid Token" });
       }
       else {
-        console.log(chalk.green("TOKEN CHECK OK"))
+        res.status(200).json({ message: "200: Success" })
         next();
       }
     })
@@ -63,25 +61,20 @@ app.get("/*", (req, res, next) => {
 app.post("/createaccount", async (req, res) => {
   try {
     const { username, email, password } = req.body
-    const salt = crypto.randomBytes(16).toString("hex")
     const hash = crypto.pbkdf2Sync(password, salt, 1000, 64, "sha256").toString("hex")
-    console.log(req.body)
     connection.query(
       "INSERT INTO `usertable3` (`username`,`email`,`password`,`salt`,`status`) VALUES (?,?,?,?,'4')",
       [username, email, hash, salt],
       (err, result) => {
         if (err) {
-          console.log("error with query", err);
-          res.status(500).send("not good");
+          res.status(500).json({ message: "500: Error with query" });
         } else if (res) {
-          console.log("success with query", result);
-          res.status(200).send("success");
+          res.status(200).json({ message: "200: Success" });
         }
       }
     );
   } catch (err) {
-    console.log(err);
-    res.status(500);
+    res.status(500).json({ message: "500: Something went wrong" });
   }
 });
 
@@ -90,24 +83,20 @@ app.post("/login", async (req, res) => {
   const loginQuery = "SELECT `email`,`password`,`salt` FROM `usertable3` WHERE `email` = ? LIMIT 1"
   connection.query(loginQuery, [req.body.email], async (err, result) => {
     if (typeof result[0]?.email === "undefined") {
-      console.log(chalk.red("LOGIN UNSUCCESSFUL, INVALID EMAIL"))
-      res.status(403).send({ message: "LOGIN UNSUCCESSFUL, INVALID EMAIL" })
+      res.status(403).send({ message: "403: Invalid email" })
       return
     }
     const { email, password, salt } = result[0]
     const comparedPass = crypto.pbkdf2Sync(typedPassword, salt, 1000, 64, "sha256").toString("hex")
     if (err) {
-      console.log(chalk.red("LOGIN QUERY UNSUCCESSFUL"), err);
-      res.status(500).send({ message: "SOMETHING WENT WRONG" });
+      res.status(500).send({ message: "500: Something went wrong" });
     }
     else if (password === comparedPass) {
       const loginToken = jwt.sign(email, process.env.P1LL_SECRETTOKEN)
-      console.log(chalk.green("LOGIN SUCCESSFUL"))
-      res.status(200).send({ message: "LOGIN SUCCESSFUL", loginToken: loginToken });
+      res.status(200).send({ message: "200: Success", loginToken: loginToken });
     }
     else {
-      console.log(chalk.red("LOGIN UNSUCCESSFUL"));
-      res.status(403).send({ message: "LOGIN UNSUCCESSFUL, INVALID CREDENTIALS" });
+      res.status(403).send({ message: "403: Invalid Credentials" });
     }
   })
 })
@@ -118,44 +107,37 @@ app.post("/deleteaccount", async (req, res) => {
   const typedPassword = req.body.delAccount
   jwt.verify(token, secret, (err, decoded) => {
     if (err) {
-      console.log(chalk.red("ERR 403 - TOKEN - INVALID"))
-      res.status(403).send({ message: "DELETE ACCOUNT QUERY FAILED INVALID TOKEN" })
+      res.status(403).send({ message: "403: Invalid token" })
     }
     else if (!passwordPattern.test(typedPassword)) {
-      console.log(chalk.red("ERR 409 - ACCOUNT - CHECK FOR REGEX"))
-      res.status(409).send({ message: "DELETE ACCOUNT QUERY FAILED PASSWORD INVALID" })
+      res.status(409).send({ message: "409: Illegal password" })
     }
     else {
       const email = decoded
       const confirmPasswordQuery = "SELECT `email`,`password`,`salt` FROM `usertable3` WHERE `email` = ?"
       connection.query(confirmPasswordQuery, [email], async (err, result) => {
         if (typeof result[0]?.email === "undefined") {
-          console.log(chalk.red("ERR 403 - ACCOUNT - NO MATCHING EMAIL"))
-          res.status(403).send({ message: "DELETE ACCOUNT QUERY FAILED - NO ACCOUNTS MATCHED EMAIL" })
+          res.status(403).send({ message: "403: Invalid email" })
           return
         }
         const { email, password, salt } = result[0]
         const comparedPass = crypto.pbkdf2Sync(typedPassword, salt, 1000, 64, "sha256").toString("hex")
         if (err) {
-          console.log(chalk.red("ERR 500 - ACCOUNT - QUERY ERROR"))
-          res.status(500).send({ message: "DELETE ACCOUNT QUERY FAILED - SERVER ERROR" })
+          res.status(500).send({ message: "500: Something went wrong - (Query)" })
         }
         else if (password === comparedPass) {
           const deleteAccountQuery = "DELETE FROM `usertable3` WHERE `email` = ?"
           connection.query(deleteAccountQuery, [email], async (err, results) => {
             if (err) {
-              console.log(chalk.red("ERR 500 - ACCOUNT - SOMETHING WENT WRONG"))
-              res.status(500).send({ message: "DELETE ACCOUNT QUERY FAILED - AN ERROR OCCURRED" })
+              res.status(500).send({ message: "500: Something went wrong - Query" })
             }
             else {
-              console.log(chalk.green("ACCOUNT SUCCESSFULLY DELETED"))
-              res.status(200).send({ message: "YOUR ACCOUNT HAS BEEN DELETED" })
+              res.status(200).send({ message: "200: Success" })
             }
           })
         }
         else {
-          console.log(chalk.red("ERR 403 - ACCOUNT - INCORRECT PASSWORD"))
-          res.status("DELETE ACCOUNT QUERY FAILED - INVALID PASSWORD")
+          res.status(403).send({ message: "403: Invalid password" })
         }
       })
     }
@@ -168,54 +150,44 @@ app.post("/changepassword", async (req, res) => {
   const { oldPassword, newPassword1, newPassword2 } = req.body
   jwt.verify(token, secret, (err, decoded) => {
     if (err) {
-      console.log(chalk.red("ERR 403 - TOKEN - INVALID"))
-      res.status(403).send({ message: "CHANGE PASSWORD QUERY FAILED INVALID TOKEN" })
+      res.status(403).send({ message: "403: Invalid token" })
     }
     else if (!passwordPattern.test(oldPassword) || !passwordPattern.test(newPassword1) || !passwordPattern.test(newPassword2)) {
-      console.log(chalk.red("ERR 409 - PASSWORD - CHECK FOR REGEX"))
-      res.status(409).send({ message: "CHANGE PASSWORD QUERY FAILED INVALID FIELDS" })
+      res.status(409).send({ message: "409: Illegal password" })
     } 
     else if (oldPassword === newPassword1 || oldPassword === newPassword2) {
-      console.log(chalk.red("ERR 409 - PASSWORD - OLD PASSWORD MATCHES NEW"))
-      res.status(409).send({ message: "CHANGE PASSWORD QUERY FAILED OLD PASSWORD MATCHES NEW PASSWORD" })
+      res.status(409).send({ message: "409: Old password matches new" })
     }
     else if (newPassword1 !== newPassword2) {
-      console.log(chalk.red("ERR 409 - PASSWORD - NEW PASSWORDS DO NOT MATCH"))
-      res.status(409).send({ message: "CHANGE PASSWORD QUERY FAILED NEW PASSWORDS DO NOT MATCH" })
+      res.status(409).send({ message: "409: New password fields do not match" })
     }
     else {
       const email = decoded
       const confirmPasswordQuery = "SELECT `email`,`password`,`salt` FROM `usertable3` WHERE `email` = ?"
       connection.query(confirmPasswordQuery, [email], async (err, result) => {
         if (typeof result[0]?.email === "undefined") {
-          console.log(chalk.red("ERR 403 - PASSWORD - NO MATCHING EMAIL"))
-          res.status(403).send({ message: "CHANGE PASSWORD QUERY FAILED BECAUSE EMAIL DID NOT MATCH" })
+          res.status(403).send({ message: "403: Invalid email" })
           return
         }
         const { email, password, salt } = result[0]
         const comparedPass = crypto.pbkdf2Sync(oldPassword, salt, 1000, 64, "sha256").toString("hex")
         if (err) {
-          console.log(chalk.red("ERR 500 - PASSWORD - QUERY ERROR"))
-          res.status(500).send({ message: "CHANGE PASSWORD QUERY FAILED - ERROR WITH SERVER" })
+          res.status(500).send({ message: "500: Something went wrong (Query)" })
         }
         else if (password === comparedPass) {
-          const salt = crypto.randomBytes(16).toString("hex")
           const hash = crypto.pbkdf2Sync(newPassword1, salt, 1000, 64, "sha256").toString("hex")
           const changePasswordQuery = "UPDATE `usertable3` SET `password` = ?, `salt` = ? WHERE `email` = ? LIMIT 1"
           connection.query(changePasswordQuery, [hash, salt, email], async (err, results) => {
             if (err) {
-              console.log(chalk.red("ERR 500 - PASSWORD - SOMETHING WENT WRONG"))
-              res.status(500).send({ message: "CHANGE PASSWORD QUERY FAILED - SOMETHING WENT WRONG" })
+              res.status(500).send({ message: "500: Something went wrong - Query" })
             }
             else {
-              console.log(chalk.green("CHANGE PASSWORD SUCCESS!"))
-              res.status(200).send({ message: "YOU HAVE SUCCESSFULLY CHANGED YOUR PASSWORD" })
+              res.status(200).send({ message: "200: Success" })
             }
           })
         }
         else {
-          console.log(chalk.red("ERR 403 - PASSWORD - INCORRECT PASSWORD"))
-          res.status(403).send({ message: "CHANGE PASSWORD QUERY FAILED - INCORRECT PASSWORD" })
+          res.status(403).send({ message: "403: Invalid Password" })
         }
       })
     }
@@ -228,30 +200,26 @@ app.post("/changeusername", async (req, res) => {
   const username = req.body.username
   jwt.verify(token, secret, (err, decoded) => {
     if (err) {
-      console.log(chalk.red("TOKEN AUTH FAIL - INVALID TOKEN"))
-      res.status(403).send({ message: "CHANGE USER QUERY FAILED INVALID TOKEN" })
+      res.status(403).send({ message: "403: Invalid token" })
     }
     else if (!usernamePattern.test(username)) {
-      console.log(chalk.red("USERNAME LENGTH INVALID"))
+      res.status(409).send({ message: "409: Illegal username" })
     }
     else {
       const isUsernameUnique = "SELECT `username` FROM `usertable3` WHERE `username` = ?"
       connection.query(isUsernameUnique, [username], async (err, usernames) => {
         if (usernames.length !== 0) {
-          console.log(chalk.red("USERNAME TAKEN"))
-          res.status(409).send({ message: "THIS USERNAME IS ALREADY TAKEN" })
+          res.status(409).send({ message: "409: Username already taken" })
         }
         else {
           const email = decoded
           const changeUsernameQuery = "UPDATE `usertable3` SET `username` = ? WHERE `email` = ? LIMIT 1"
           connection.query(changeUsernameQuery, [username, email], async (err, result) => {
             if (err) {
-              console.log(chalk.red("ERROR WITH CHANGE USER QUERY", err))
-              res.status(500).send({ message: "CHANGE USER QUERY FAILED" })
+              res.status(500).send({ message: "500: Something went wrong - (Query)" })
             }
             else if (result) {
-              console.log(chalk.green("CHANGED USERNAME!"))
-              res.status(200).send({ message: "YOUR USERNAME WAS CHANGED" })
+              res.status(200).send({ message: "200: Success" })
             }
           })
         }
@@ -260,9 +228,9 @@ app.post("/changeusername", async (req, res) => {
   })
 })
 
-app.post("/logout", async (req, res) => {
+/* app.post("/logout", async (req, res) => {
 
-})
+}) */
 
 app.listen(port);
 console.log(chalk.bgCyan(`LISTENING ON (${port})`));
