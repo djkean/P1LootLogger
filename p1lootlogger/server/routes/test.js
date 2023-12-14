3dabf63668ede7eb9bedc3b51718c41882c40afa
djkean <djkean7@gmail.com>
2023-12-14 15:12:12 -0330

    Making /verifyuser look better
    - Gave styling to front end for /verifyuser
    - Implemented conditional messages for the user based on the success/failure in trying to verify their account
    - Added an additional case in backend for /verifyuser for if a user is already verified when they attempt to verify their account
    - Removed numerous console logs from backend for /verifyuser as they are no longer needed

--- p1lootlogger/server/routes/test.js
+++ p1lootlogger/server/routes/test.js
@@ -45,27 +45,26 @@ router.get("/bossdetails", (req, res) => {
 router.post("/verifyemail", (req, res) => {
   console.log(req.body)
   const {token, email} = req.body
-  const verifyEmailQuery = "SELECT `email`, `token` FROM `usertable4` WHERE `email` = ? LIMIT 1"
+  const verifyEmailQuery = "SELECT `email`, `token`, `status` FROM `usertable4` WHERE `email` = ? LIMIT 1"
   connection.query(verifyEmailQuery, [email], (err, result) => {
     if (err) {
-      console.log(err, "something went wrong with verification query")
-      res.json({ status: 500, error: "verification query failed", response: null })
-    }
+      res.json({ status: 500, error: "Verification failed, please try again", response: null })
+    } 
     else if (result[0].token === token) {
-      const updateStatusQuery = "UPDATE `usertable4` SET `status` = 4 WHERE `email` = ?"
-      connection.query(updateStatusQuery, [email], (error, response) => {
-        if (error) {
-          console.log("Something went wrong when updating user status")
-          res.json({ status: 500, error: "Something went wrong", response: null })
-        }
-        else {
-          console.log("updated user status, account is verified")
-          res.json({ status: 200, error: null, response: "success (status)" })
-        }
-      })
+      if (result[0].status === 4) {
+        res.json({ status: 409, error: "Your account has already been verified", response: null })
+      } else {
+        const updateStatusQuery = "UPDATE `usertable4` SET `status` = 4 WHERE `email` = ?"
+        connection.query(updateStatusQuery, [email], (error, response) => {
+          if (error) {
+            res.json({ status: 500, error: "Something went wrong, please try again", response: null })
+          } else {
+            res.json({ status: 200, error: null, response: "Your account is now verified" })
+          }
+        })
+      }
     } else {
-      console.log("token provided did not match any user")
-      res.json({ status: 403, error: "verification failed", response: null })
+      res.json({ status: 403, error: "Verification failed. Invalid Token", response: null })
     }
   })
 })
--- p1lootlogger/src/pages/verifyemail.jsx
+++ p1lootlogger/src/pages/verifyemail.jsx
@@ -1,16 +1,18 @@
 import React, { useEffect, useState } from "react";
-import { useLocation } from "react-router-dom";
-import axios from "axios";
- 
+import { useLocation, Link } from "react-router-dom";
+import { Box, Flex, Heading, Stack, Text } from "@chakra-ui/react";
+import { LoginBox, LoginFlex, LoginStack } from "../components/pagestyles"
+
 export const VerifyEmailPage = (res) => {
   const [responseFromDb, setResponseFromDb] = useState({})
+
   const location = useLocation();
   const queryString = location.search
   const params = new URLSearchParams(queryString)
   const tokenFromQueryString = params.get("token")
   const emailFromQueryString = params.get("email")
 
-  console.log("this is res", tokenFromQueryString, emailFromQueryString)
+  console.log(tokenFromQueryString, emailFromQueryString)
 
   const compareStringsToDb = async () => {
     if (typeof tokenFromQueryString === "undefined" || typeof emailFromQueryString === "undefined") {
@@ -26,22 +28,46 @@ export const VerifyEmailPage = (res) => {
       })
       if (verificationResponse.ok) {
         const responseData = await verificationResponse.json()
-        console.log(responseData)
         setResponseFromDb(responseData)
       }
       else {
-       console.log("error in POST request")
+       console.log("Error in POST request")
       }
     }
     catch(err) {
-      console.log("verifyemail fetch failed", err)
+      console.log(err)
     }
   }
-
 
   useEffect(() => {
     compareStringsToDb()
   }, []);
 
-  return (<div>this is the very well developed verifyemail page</div>)
+  return (
+    <Flex sx={LoginFlex} align={"center"}>
+      <Stack sx={LoginStack} spacing={6} align={"center"}>
+        <Heading fontSize={"2xl"} py={3} marginTop={"-8.5em"} marginBottom={"-1 em"}>Verify Email</Heading>
+        <Box sx={LoginBox}>
+          {responseFromDb.response && 
+          <Stack align={"center"}>
+            <Text color={"#FDCA40"}>{responseFromDb.response}</Text>
+            <Link to="/login">
+              <Text color={"#BFA55C"}>You can click here to login</Text>
+            </Link>
+          </Stack>}
+          {responseFromDb.error && responseFromDb.status !== 409 &&
+          <Stack align={"center"}>
+            <Text color={"#FDCA40"}>{responseFromDb.error}</Text>
+          </Stack>}
+          {responseFromDb.status === 409 && 
+          <Stack align={"center"}>
+            <Text color={"#FDCA40"}>{responseFromDb.error}</Text>
+            <Link to="/login">
+              <Text color={"#BFA55C"}>You can click here to login</Text>
+            </Link>
+          </Stack>}
+        </Box>
+      </Stack>
+    </Flex>
+  )
 }
