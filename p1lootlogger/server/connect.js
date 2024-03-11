da8a831d3a226f4f51518fd985303a65e10ef12f
djkean <djkean7@gmail.com>
2024-03-11 17:42:36 -0230

    Start shell script + host env var

--- p1lootlogger/server/connect.js
+++ p1lootlogger/server/connect.js
@@ -3,7 +3,7 @@ const mysql2 = require("mysql2");
 
 // connects to local database with env variables for credentials
 const connection = mysql2.createConnection({
-  host: "localhost",
+  host: process.env.P1LL_HOST,
   user: process.env.P1LL_USER,
   password: process.env.P1LL_PASSWORD,
   database: process.env.P1LL_DATABASE,
