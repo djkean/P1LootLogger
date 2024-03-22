d4c98cb2594ba0f70127ae2467d8e005c4770076
djkean <djkean7@gmail.com>
2024-03-22 02:27:38 -0230

    Changes to /Home
    - Rewriting and improving home page

--- p1lootlogger/src/pages/home.jsx
+++ p1lootlogger/src/pages/home.jsx
@@ -1,17 +1,18 @@
 import React from "react";
+import { Box, Button, Center, Container, Flex, Heading, Image, Stack, Text } from "@chakra-ui/react";
+import { LoginBox, LoginFlex } from "../components/pagestyles";
 
 export const HomePage = () => {
-  return (
-    <div>
-      <section>
-        <p>
-         PokeOne Loot Logger is a community driven, crowdsourced tool meant 
-         to let players help players - by reporting their loot and breaking 
-         down the numbers for both them and others. You can view stats and 
-         loot tables for your favourites bosses without an account, but users 
-         looking to help report data will need to create an account first.
-        </p>
-      </section>
-    </div>
+  return ( 
+    <Flex sx={LoginFlex} align={"center"}>
+      <Stack>
+        <Heading>PokeOne LootLogger</Heading>
+        <Text>Welcome to PokeOne LootLogger, a "crowdsourced" wiki of item and boss data for PokeOne.
+          This community driven project is designed to let players help themselves by contributing and combining their information to create a reference for any activity's rewards, and the likelihood of earning those rewards.
+          You can view tables for the items and bosses of PokeOne. Each Boss has a detailed page of other player's reports of what they received from those bosses, allowing you to see personal results from each individual statistic collected by players.
+          In order to view and submit loot, you must first create and account, which you can do [here].
+        </Text>
+      </Stack>
+    </Flex>
   )
 }
